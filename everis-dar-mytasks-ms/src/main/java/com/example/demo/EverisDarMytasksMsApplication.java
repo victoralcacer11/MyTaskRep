8583f94c8a9d5581b557b88e8b9930fb6862e934
victoralcacer11 <vic11@hotmail.es>
2022-04-05 17:26:45 +0200

    creación projecto
    utilizando spring boot se ha creado el proyecto

---
+++ everis-dar-mytasks-ms/src/main/java/com/example/demo/EverisDarMytasksMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverisDarMytasksMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverisDarMytasksMsApplication.class, args);
+	}
+
+}
