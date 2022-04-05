99b652d75e6a01e75c0c67bae9d5d214be86f3b9
victoralcacer11 <vic11@hotmail.es>
2022-04-05 17:52:25 +0200

    base de datos
    actualizando propertis y creando la base de datos

---
+++ everis-dar-mytasks-ms/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Tasks (id, description, state) VALUES (1, 'CREAR BASE DE DATOS', 'En proceso');
+INSERT INTO countries (id, description, state) VALUES (2, 'Crear el FrontEnd','Sin iniciar');
+INSERT INTO countries (id, description, state) VALUES (3, 'Hacer Rehabilitacion','Finalizada');
