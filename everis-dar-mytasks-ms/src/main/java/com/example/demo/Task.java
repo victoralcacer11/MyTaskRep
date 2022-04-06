dedfcb854d4c03f2309ddfa76678a8cfc0794fa7
victoralcacer11 <vic11@hotmail.es>
2022-04-06 09:10:39 +0200

    Añadidas anotaciones de Spring

--- everis-dar-mytasks-ms/src/main/java/com/example/demo/Task.java
+++ everis-dar-mytasks-ms/src/main/java/com/example/demo/Task.java
@@ -1,9 +1,18 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Task{
 	
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long ID;
+	
 	private String description;
 	private String status;
 	
