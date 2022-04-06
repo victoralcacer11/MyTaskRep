6780b16b67b1d2886bd924ddf47f2a0348c3e796
victoralcacer11 <vic11@hotmail.es>
2022-04-06 08:54:56 +0200

    Estructura clase Task

---
+++ everis-dar-mytasks-ms/src/main/java/com/example/demo/Task.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+
+public class Task{
+	
+	private Long ID;
+	private String description;
+	private String status;
+	
+	public Task() {
+		this.description = '' ;
+		this.status = '' ;
+	}
+	
+}
+
