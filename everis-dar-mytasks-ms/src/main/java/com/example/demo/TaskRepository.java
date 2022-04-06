079cd7cfedfc346d1cf7927dee639f982b36c001
victoralcacer11 <vic11@hotmail.es>
2022-04-06 15:27:54 +0200

    creacion Controller y mejora repository
    controller vacio y añadida @Repository

--- everis-dar-mytasks-ms/src/main/java/com/example/demo/TaskRepository.java
+++ everis-dar-mytasks-ms/src/main/java/com/example/demo/TaskRepository.java
@@ -3,13 +3,14 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * CrudRepository for manage web request
  * @author valcacer
  *
  */
-
+@Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
 	
 	/**
