9d21bd3c01db008fea5fbed29e7533b40727f268
victoralcacer11 <vic11@hotmail.es>
2022-04-06 09:28:50 +0200

    getters y setters

--- everis-dar-mytasks-ms/src/main/java/com/example/demo/Task.java
+++ everis-dar-mytasks-ms/src/main/java/com/example/demo/Task.java
@@ -8,17 +8,48 @@ import javax.persistence.Id;
 @Entity
 public class Task{
 	
+	/**
+	 * ID Task unico
+	 */
 	
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long ID;
 	
+	/**
+	 * Task description
+	 */
 	private String description;
-	private String status;
 	
-	public Task() {
-		this.description = '' ;
-		this.status = '' ;
+	/**
+	 * Task status
+	 */
+	private String state;
+	
+	/**
+	 * Constructor basic Task
+	 */
+	public Task(Long ID, String description, String state) {
+		
+		this.ID = ID;
+		this.setDescription(description) ;
+		this.setState(state) ;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
 	}
 	
 }
