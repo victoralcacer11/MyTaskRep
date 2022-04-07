886fb2c29493c456156d51fd11844cf5f16b0a6a
victoralcacer11 <vic11@hotmail.es>
2022-04-07 11:23:11 +0200

    creado metodo addTask
    Falta  completar su funcionamiento

--- everis-dar-mytasks-ms/src/main/java/com/example/demo/TaskController.java
+++ everis-dar-mytasks-ms/src/main/java/com/example/demo/TaskController.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,6 +58,11 @@ public class TaskController{
 		}	
 			
 	    return tasksEnd;
+	}
+	
+	@PostMapping
+	public Task addTask() {
+		return null;
 	}
 
 
