6cbdf52e2ce4fb93080b85e837cb49c4ea2f3e8f
victoralcacer11 <vic11@hotmail.es>
2022-04-08 10:57:34 +0200

    Modificado test main y creada clase del test controller

--- everis-dar-mytasks-ms/src/test/java/com/example/demo/EverisDarMytasksMsApplicationTests.java
+++ everis-dar-mytasks-ms/src/test/java/com/example/demo/EverisDarMytasksMsApplicationTests.java
@@ -2,37 +2,19 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 @SpringBootTest
 class EverisDarMytasksMsApplicationTests {
 
-	@Autowired
-	TaskController tcon;
-	
-	@MockBean
-	TaskRepository trepo;
 	
 	@Test
 	void contextLoads() {
 	}
 	
 	@Test
-	public void addTaskTest() {
-		Task testTask = tcon.addTask(buildTask());
-		System.out.println(testTask);
-	//	assertEquals(testTask,201);
-		
-	}
-	
-	
-	
-	public Task buildTask() {
-		Task t= new Task();
-		t.setDescription("estoy haciendo el test");
-		t.setState("En proceso");
-		return t;
-		
+	void main() {
+		final String[] args = new String[0];
+		EverisDarMytasksMsApplication.main(args);
 	}
 }
---
+++ everis-dar-mytasks-ms/src/test/java/com/example/demo/TaskControllerTest.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.example.demo;
+
+/**
+ * @author valcacer
+ *
+ */
+public class TaskControllerTest {
+
+}
