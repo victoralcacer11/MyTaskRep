c53ef42a83f60d9aea7e64a8cf18bc2c8d67fd69
victoralcacer11 <vic11@hotmail.es>
2022-04-08 11:48:20 +0200

    creando controllerTest
    primeros pasos, creacion de un metodo para antes del test en si

--- everis-dar-mytasks-ms/src/test/java/com/example/demo/TaskControllerTest.java
+++ everis-dar-mytasks-ms/src/test/java/com/example/demo/TaskControllerTest.java
@@ -3,10 +3,52 @@
  */
 package com.example.demo;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvc;
+
 /**
  * @author valcacer
  *
  */
+@ExtendWith(MockitoExtension.class)
 public class TaskControllerTest {
+	
+	private MockMvc mvc;
+	/**
+	 * controller to tst
+	 */
+	@InjectMocks
+	private TaskController taskController;
+	/**
+	 * 
+	 */
+	@Mock
+	private TaskRepository taskRepository;
+	/**
+	 * task object to use in the test
+	 * Task properties to test
+	 */
+	private Task task;
+	private String taskDescription = "Task test";
+	private String taskState = "Iniciada";
+	
+	/**
+	 * Method lunch before @test
+	 * this create a task and buid
+	 */
+	@BeforeEach
+	public void setUp() {
+		this.task = new Task();
+		this.task.setDescription(taskDescription);
+		this.task.setState(taskState);
+		;
+		
+	}
+	
+	
+	
 
 }
